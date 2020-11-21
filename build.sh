ccdbf338bfec93c93338126d4029a685543f4cdd
DavidDeng <daviddenghaotian@163.com>
2020-11-21 15:23:48 -0500

    added confirmation in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+# a function to get user confirmation
+confirm() {
+	local answer
+	local message=${1-"Are you sure?"}
+	echo "$message [Y/N] => "
+	read -n 1 -s answer
+	case $answer in
+		[yY])
+			return 0
+			;;
+		*)
+			return -1
+			;;
+	esac
+}
+
+if [ $# -ne 1 ]; then
+	echo "Usage $0 [animation_script.json]"
+	exit -1
+fi
 animation_script=$1
 if [ ! -f "$animation_script" ]; then
 	echo $animation_script does not exist.
@@ -11,22 +32,41 @@ video_path=scenes/$animation_name/output.mp4
 
 echo animation_name: $animation_name
 
-mkdir -p $scene_data_dir
-echo building frame data...
-echo python3 build_frames.py $animation_script $scene_data_dir
-python3 build_frames.py $animation_script $scene_data_dir
-echo frame data written to $scene_data_dir
+if [ ! -d $scene_data_dir ] ||
+	confirm "$scene_data_dir already has $(ls -l $scene_data_dir | wc -l) frame files. Rebuild frame data?"; then
+	mkdir -p $scene_data_dir
+	echo building frame data...
+	echo python3 build_frames.py $animation_script $scene_data_dir
+	python3 build_frames.py $animation_script $scene_data_dir
+	if [ $? -ne 0 ]; then
+		echo failed building frame data.
+		exit -2
+	fi
+	echo frame data written to $scene_data_dir
+fi
 
-mkdir -p $scene_images_dir
-echo building frame images...
-echo dart raytrace.dart $scene_data_dir $scene_images_dir
-dart raytrace.dart $scene_data_dir $scene_images_dir
-echo frame images written to $scene_images_dir
+if [ ! -d $scene_images_dir ] ||
+	confirm "$scene_images_dir already has $(ls -l $scene_images_dir | wc -l) frame images. Rebuild frame images?"; then
+	mkdir -p $scene_images_dir
+	echo building frame images...
+	echo dart raytrace.dart $scene_data_dir $scene_images_dir
+	dart raytrace.dart $scene_data_dir $scene_images_dir
+	if [ $? -ne 0 ]; then
+		echo failed building frame images.
+		exit -2
+	fi
+	echo frame images written to $scene_images_dir
+fi
 
-echo building video...
-echo ffmpeg -r 1 -pattern_type glob -i "${scene_images_dir}/*.ppm" \
-       -vcodec libx264 -crf 15 -pix_fmt yuv420p $video_path
-ffmpeg -r 1 -pattern_type glob -i "${scene_images_dir}/*.ppm" \
-       -vcodec libx264 -crf 15 -pix_fmt yuv420p $video_path
-echo video written to $video_path
-
+if confirm "Build video to $video_path?"; then
+	echo building video...
+	echo ffmpeg -r 1 -pattern_type glob -i "${scene_images_dir}/*.ppm" \
+		   -vcodec libx264 -crf 15 -pix_fmt yuv420p $video_path
+	ffmpeg -r 1 -pattern_type glob -i "${scene_images_dir}/*.ppm" \
+		   -vcodec libx264 -crf 15 -pix_fmt yuv420p $video_path
+	if [ $? -ne 0 ]; then
+		echo failed building video.
+		exit -2
+	fi
+	echo video written to $video_path
+fi
